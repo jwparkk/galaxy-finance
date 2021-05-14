86a5c95595c82f5f23236a69630feffcfcb4a4c8
jumanzi <jumanzi.dev@gmail.com>
2021-05-15 03:31:52 +0900

    Init GalaxyController contract

--- contracts/GalaxyController.sol
+++ contracts/GalaxyController.sol
@@ -1,12 +1,26 @@
 /// SPDX-License-Identifier: <SPDX-License>
 pragma solidity 0.8.4;
 
+/**
+@title GalaxyController
+In Galaxy trading System, we controll three types of prediction pool (1,3,7) prediction
+In each prediction pool, there are 'articles' on both long/short position.
+A user can support any article by voting 'GALA' token.
+ */
 contract GalaxyController {
-    struct Predictions {
+    struct UserInfo {
+        uint256 amount; // How many LP tokens the user has provided.
+        bool position; // true: up vote false: down vote
+    }
+
+    struct Article {
         address writer;
+        uint256 votes;
+    }
+
+    struct Pool {
+        uint256 totalVotes;
         uint256 endTime;
-        uint256 upVoteCount;
-        uint256 downVoteCount;
     }
 
     //Dev address
@@ -14,5 +28,14 @@ contract GalaxyController {
 
     constructor(address _dev) {
         dev = _dev;
+    }
+
+    // ...TODO...
+    function getVotingCount(
+        uint256 _pid,
+        uint256 _amount,
+        bool vote
+    ) public view {
+        return;
     }
 }
